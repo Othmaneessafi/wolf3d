58e29c4343221d93a0616bff8bf554b6e8f611c6
ChadiNaour <naourchadi@gmail.com>
2020-11-27 00:25:23 +0100

    flor/ceiling

--- main.c
+++ main.c
@@ -1,78 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnaour <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 01:24:42 by cnaour            #+#    #+#             */
+/*   Updated: 2020/10/18 01:27:18 by cnaour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "wolf3d.h"
 
-void    init_wolf(wolf_t *wolf)
+void		init_wolf(t_wolf *wolf)
 {
-    wolf->window = NULL;
-    wolf->renderer = NULL;
-    wolf->loop = 0;
-    wolf->tickslastframe = 0;
-    wolf->colorbuffer =  (int*)malloc(sizeof(int) * (int)WIDTH * (int)HEIGHT);
-    //wolf->colorbuffertexture = SDL_CreateTexture(wolf->renderer, SDL_PIXELFORMAT_ABGR8888,
-      //                              SDL_TEXTUREACCESS_STREAMING ,WIDTH, HEIGHT );
-    wolf->walltex = (int*) malloc(sizeof(int) * (int) tex_w * (int) tex_h);
+	wolf->window = NULL;
+	wolf->renderer = NULL;
+	wolf->loop = 0;
+	wolf->tickslastframe = 0;
+	wolf->colorbuffer = (int *)malloc(sizeof(int)
+					* WIDTH * HEIGHT);
 }
 
-void    init_player(player_t *player, wolf_t *wolf)
+void		init_player(t_player *player, t_wolf *wolf)
 {
-    player->x = WIDTH / 2;
-    player->y = HEIGHT / 2;
-    player->width = 2;
-    player->height = 32;
-    player->turndirection = 0;
-    player->walkdirection = 0;
-    player->rotatangle = PI;
-    player->walkspeed = 100;
-    player->turnspeed = 70 * (PI / 180);
-    player->colorbuffertexture = SDL_CreateTexture(wolf->renderer, SDL_PIXELFORMAT_ARGB8888,
-                                    SDL_TEXTUREACCESS_STREAMING ,WIDTH, HEIGHT );
-    
+	player->x = WIDTH / 2;
+	player->y = HEIGHT / 2;
+	player->width = 2;
+	player->height = 32;
+	player->turndirection = 0;
+	player->walkdirection = 0;
+	player->rotatangle = PI;
+	player->walkspeed = 200;
+	player->turnspeed = 70 * (PI / 180);
+	player->colorbuffertexture = SDL_CreateTexture(wolf->renderer,
+			SDL_PIXELFORMAT_ARGB8888,
+			SDL_TEXTUREACCESS_STREAMING
+			, WIDTH, HEIGHT);
 }
 
-void    init_tab(ray_t *rays)//[NUM_RAY])
+void		init_tab(t_ray *rays)
 {
-    int j = 0;
-    while (j < NUM_RAY)
-    {
-        rays[j].distance = 0;
-        rays[j].hitver = 0;
-        rays[j].rayangle = 0;
-        rays[j].raydown = 0;
-        rays[j].rayleft = 0;
-        rays[j].rayright = 0;
-        rays[j].rayup = 0;
-        rays[j].wallhitcontent = 0;
-        rays[j].wallhitx = 0;
-        rays[j].wallhity = 0;
-        j++;
-    }
+	int j;
+
+	j = 0;
+	while (j < NUM_RAY)
+	{
+		rays[j].distance = 0;
+		rays[j].hitver = 0;
+		rays[j].rayangle = 0;
+		rays[j].raydown = 0;
+		rays[j].rayleft = 0;
+		rays[j].rayright = 0;
+		rays[j].rayup = 0;
+		rays[j].wallhitcontent = 0;
+		rays[j].wallhitx = 0;
+		rays[j].wallhity = 0;
+		j++;
+	}
 }
 
-int main(int ac, char **av)
+int			main(int ac, char **av)
 {
-    wolf_t wolf;
-    player_t player;
-    ray_t   rays[NUM_RAY];
+	t_wolf		wolf;
+	t_player	player;
+	t_ray		rays[NUM_RAY];
 
-    int **map;
-    map = ft_check(ac, av);
-    if (map == 0)
-    {
-        printf("map is invalid !!!\n");
-        return (0);
-    }
-    init_wolf(&wolf);
-    wolf.loop = init_window(&wolf);
-    imgs(&wolf);
-    init_tab(rays);
-    //gun(&wolf, &player);
-    tex2(&wolf, &player);
-    init_player(&player, &wolf);
-    (void)map;
-    while (wolf.loop)
-    {
-        process_input(&wolf, &player);
-        update(map, &wolf.tickslastframe, &player, rays);
-        render(&wolf, &player, map, rays);
-    }
-    destroy_window(&wolf);
+	wolf.map = ft_check(ac, av);
+	if (wolf.map == 0)
+	{
+		printf("map is invalid !!!\n");
+		return (0);
+	}
+	init_wolf(&wolf);
+	wolf.loop = init_window(&wolf);
+	imgs(&wolf);
+	init_tab(rays);
+	tex2(&wolf);
+	init_player(&player, &wolf);
+	while (wolf.loop)
+	{
+		process_input(&wolf, &player);
+		update(wolf.map, &wolf.tickslastframe, &player, rays);
+		render(&wolf, &player, wolf.map, rays);
+	}
+	destroy_window(&wolf);
 }
--- wolf3d.h
+++ wolf3d.h
@@ -1,34 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnaour <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/18 22:11:30 by cnaour            #+#    #+#             */
+/*   Updated: 2020/10/18 22:39:12 by cnaour           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WOLF3D_H
 # define WOLF3D_H
 
-//constants
 # define TRUE 1
 # define FALSE 0
 # define PI 3.14159265
 # define TWO_PI 6.28318530
-//map
 # define TILE_SIZE 64
 # define ROWS 13
 # define COLS 20
-//angle
 # define FOVA (60 * (PI / 180))
-//window
 # define WIDTH (COLS * TILE_SIZE)
 # define HEIGHT (ROWS * TILE_SIZE)
-//time and frames
 # define FPS 30
 # define FRAME_TIME (1000/FPS)
-//rays
 # define WALL_STRIP_WIDTH 1
 # define NUM_RAY WIDTH / WALL_STRIP_WIDTH
-//scaling
 # define MM_SCALE 0.2
+# define TEX_H 64
+# define TEX_W 64
+# define CENTERY HEIGHT / 2
 
-//textures
-# define tex_w 64
-# define tex_h 64
 
-//includes
 # include <math.h>
 # include <unistd.h>
 # include <stdlib.h>
@@ -41,98 +45,112 @@
 # include <fcntl.h>
 # include "Get_Next_Line/get_next_line.h"
 # include "Get_Next_Line/libft/libft.h"
-# include "libpng/upng.h"
 
-//game struct
-typedef struct      wolf_s
+typedef struct			s_wolf
 {
-    SDL_Window      *window;
-    SDL_Renderer    *renderer;
-    SDL_Surface     *surface;
-    SDL_Texture     *text;
-    int             loop;
-    float           tickslastframe;
-    int*            colorbuffer;
-    int*            walltex;
-    int*            walltex1;
-    int*            walltex2;
-    int*            walltex3;
-    int*            walltex4;
-    upng_t*         pngTex;
-    char            **pics;
-    char            *img;
-    //SDL_Texture     *colorbuffertexture;
+	SDL_Window		*window;
+	SDL_Renderer	*renderer;
+	SDL_Surface     *surface;
+	SDL_Texture     *text;
+	int				loop;
+	float			tickslastframe;
+	int				*colorbuffer;
+	int				**map;
+	char 			**pics;
+	int 			*walltex;
+	int 			*walltex1;
+	int 			*walltex2;
 
-}                   wolf_t;
+}						t_wolf;
 
-//player struct
-typedef struct      player_s
+typedef struct			s_player
 {
-    float           x;
-    float           y;
-    float           width;
-    float           height;
-    int             turndirection;//-1 left. 1 right
-    int             walkdirection;
-    float           rotatangle;
-    float           walkspeed;
-    float           turnspeed;
-    SDL_Texture     *colorbuffertexture;
-}                   player_t;
+	float				x;
+	float				y;
+	float			width;
+	float			height;
+	int				turndirection;
+	int				walkdirection;
+	float			rotatangle;
+	float			walkspeed;
+	float			turnspeed;
+	SDL_Texture		*colorbuffertexture;
+}						t_player;
 
-//ray struct
-typedef struct      ray_s
+typedef struct			s_texture
 {
-    int     stripId;
-    float   rayangle;
-    float   wallhitx;
-    float   wallhity;
-    float   distance;
-    int     hitver;
-    int     rayup;
-    int     raydown;
-    int     rayleft;
-    int     rayright;
-    int     wallhitcontent;
-}                   ray_t;
+	int				offx;
+	int 			offy;
+	unsigned int 	color;
+	float			ratio;
+	int				diago_distance;
+	float			endx;
+	float			endy;
+	int				cellx;
+	int				celly;
+	int				tilerow;
+	int				tilecol;
+}						t_texture;
 
-//raycast results
-typedef struct      results_s 
+typedef struct			s_ray
 {
-    int     foundhit;
-    float   wallhitx;
-    float   wallhity;
-    int     content;
+	int		stripid;
+	float	rayangle;
+	float	wallhitx;
+	float	wallhity;
+	float	distance;
+	int		hitver;
+	int		rayup;
+	int		raydown;
+	int		rayleft;
+	int		rayright;
+	int		wallhitcontent;
+}						t_ray;
 
-}                   results_t;
-
-typedef struct      texture_s 
+typedef struct			s_results
 {
-    unsigned int    color;
-    float           ratio;
-    int             yend;
-    int             xend;
-    int             diagonaldistance;
-    int             cellx;
-    int             celly;
-    int             tilerow;
-    int             tilecol;
-}                   texture_t;
+	int		foundhit;
+	float	wallhitx;
+	float	wallhity;
+	int		content;
+}						t_results;
 
+typedef struct			s_cords
+{
+	float		x;
+	float		y;
+}						t_cords;
 
-int     init_window(wolf_t *wolf);
-void    destroy_window(wolf_t *wolf);
-void    process_input(wolf_t *wolf, player_t *player);
-void    render(wolf_t   *wolf, player_t *player, int **map, ray_t *rays);//[NUM_RAY]);
-void    update(int **map, float *ticks, player_t *player, ray_t *rays);//[NUM_RAY]);
-float   normalizeangle(float angle);
-float   distancecalc(float x1, float y1, float x2, float y2);
-int     mapwall(int **map, float x, float y);
-void    castallrays(player_t *player, int **map, ray_t *ray);//[NUM_RAY]);
-int     **ft_check(int ac, char **av);
-void    tex(wolf_t *wolf);
-void    tex2(wolf_t *wolf, player_t *p);
-void    imgs(wolf_t *wolf);
-void    gun(wolf_t *wolf, player_t *p);
+typedef struct			s_projection
+{
+	float	perpdistance;
+	float	distprojplane;
+	float	wallprojheight;
 
+}						t_projection;
+
+int						init_window(t_wolf *wolf);
+void					destroy_window(t_wolf *wolf);
+void					process_input(t_wolf *wolf, t_player *player);
+void					render(t_wolf *wolf, t_player *player, int **map,
+		t_ray *rays);
+void					update(int **map, float *ticks, t_player *player,
+		t_ray *rays);
+float					normalizeangle(float angle);
+float					distancecalc(float x1, float y1, float x2, float y2);
+int						mapwall(int **map, float x, float y);
+void					castallrays(t_player *player, int **map, t_ray *ray);
+int						**ft_check(int ac, char **av);
+void					clearcolorbuffer(int *buffer, int color);
+void					rendercolorbuffer(t_player *p, t_wolf *wolf);
+void					generate3dprojection(t_ray *rays, t_player *player,
+		t_wolf *wolf);
+void					init_raycasting_results(t_results *res);
+t_results				looping_hor_cast(t_cords intercept, t_cords step,
+		t_ray *ray, int **map);
+t_results				looping_ver_cast(t_cords intercept, t_cords step,
+		t_ray *ray, int **map);
+void    imgs(t_wolf *wolf);
+void    tex2(t_wolf *wolf);
+void		convert_data_img(int *tab, int range);
 #endif
